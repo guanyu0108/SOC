6f02e3d121bc9683709f9d2e2a4b38b9c824d701
GuanYu <chen.guan.yu.0108@gmail.com>
2023-09-30 12:01:11 +0800

    add tb content

--- tb.v
+++ tb.v
@@ -1,5 +1,27 @@
+`timescale 1ns/1ps
 module tb;
-    
+    reg x, y;
+    wire s, c;
 
+    initial begin
+    x = 0;
+    y = 0;
+    #5;
+    $display("s=%d, c=%d", s, c);
+    x = 1;
+    y = 0;
+    #5;
+    $display("s=%d, c=%d", s, c);
+    x = 0;
+    y = 1;
+    #5;
+    $display("s=%d, c=%d", s, c);
+    x = 1;
+    y = 1;
+    #5;
+    $display("s=%d, c=%d", s, c);    
+
+    $finish;
+    end
     half_adder DUT(x, y, s, c);
 endmodule
